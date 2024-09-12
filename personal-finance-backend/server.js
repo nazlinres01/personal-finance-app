a5c47beea2617f4690e1de0d7b7ae8cbb13de9cb
nazli <nazlinuresmeray049@gmail.com>
2024-09-12 18:56:36 +0300

    .env added

--- personal-finance-backend/server.js
+++ personal-finance-backend/server.js
@@ -1,12 +1,22 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Veritabanı bağlantısı
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => console.log('Connected to MongoDB'))
+  .catch(err => {
+    console.error('Failed to connect to MongoDB', err);
+    process.exit(1);
+  });
+
 // JSON verilerini işlemek için middleware
 app.use(express.json());
-
-// API route'larını buraya ekleyin (örneğin: /api/transactions)
 
 // Statik dosyaları sunmak için
 app.use(express.static(path.join(__dirname, 'personal-financial-frontend', 'build')));
--- personal-finance-frontend/src/services/api.js
+++ personal-finance-frontend/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-const API_URL = '/api/transactions'; // Proxy kullanarak backend'e istek yönlendiriliyor
+const API_URL = process.env.REACT_APP_API_URL;
 
-export const getTransactions = () => axios.get(API_URL);
-export const addTransaction = (transaction) => axios.post(API_URL, transaction);
-export const updateTransaction = (id, transaction) => axios.put(`${API_URL}/${id}`, transaction);
-export const deleteTransaction = (id) => axios.delete(`${API_URL}/${id}`);
+export const getTransactions = () => axios.get(`${API_URL}/transactions`);
+export const addTransaction = (transaction) => axios.post(`${API_URL}/transactions`, transaction);
+export const updateTransaction = (id, transaction) => axios.put(`${API_URL}/transactions/${id}`, transaction);
+export const deleteTransaction = (id) => axios.delete(`${API_URL}/transactions/${id}`);
